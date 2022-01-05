b95781170a73d596e28e0859179809fb9a9793f6
Suyash <shrivastavasuyash15@icloud.com>
2022-01-05 14:37:25 +0530

    final

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
+import {useState} from 'react';
+import CartProvider from "./store/CartProvider";
+
+function App() {
+  const [cartIsShown, setCardIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setCardIsShown(true)
+  }
+  
+  const hideCartHandler = () => {
+    setCardIsShown(false)
+  }
+
+  return (
+    <CartProvider>
+     {cartIsShown && <Cart onClose={hideCartHandler}></Cart>}
+    <Header onShowCart={showCartHandler}></Header>
+    <main>
+      <Meals></Meals>
+    </main>
+    </CartProvider>
+  );
+}
+
+export default App;
